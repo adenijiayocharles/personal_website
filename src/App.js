7e5d76f4bcc043807cdc1d25fbc907feeacc1d9e
Adeniji Charles Ayodipupo <adenijiayocharles@gmail.com>
2020-06-20 00:24:47 +0100

    update readme

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-         Expe
+         Expect awesomeness here soon
       </header>
     </div>
   );
